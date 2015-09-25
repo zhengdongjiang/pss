ff0d36a0dbd002965a7cba90b7e3882057f1e276
vakinge <jiangw.sky@qq.com>
2015-09-25 16:48:37 +0800

    增加异常捕捉

--- pss-proxy/src/main/java/com/lifesense/pss/resolver/SubscriptionScheduler.java
+++ pss-proxy/src/main/java/com/lifesense/pss/resolver/SubscriptionScheduler.java
@@ -2,14 +2,20 @@ package com.lifesense.pss.resolver;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import kafka.consumer.KafkaStream;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.concurrent.DefaultManagedAwareThreadFactory;
+
+import kafka.consumer.KafkaStream;
+import scala.concurrent.impl.ExecutionContextImpl.DefaultThreadFactory;
 
 /**
  * @author ZengFC
@@ -17,7 +23,7 @@ import org.slf4j.LoggerFactory;
  */
 public class SubscriptionScheduler {
 	private ThreadPoolExecutor executor;
-	private Logger logger = LoggerFactory.getLogger(SubscriptionScheduler.class);
+	private static Logger logger = LoggerFactory.getLogger(SubscriptionScheduler.class);
 	private ListenerResolver listenerResolver;
 
 	public void destory() {
@@ -47,8 +53,17 @@ public class SubscriptionScheduler {
 		//executor = Executors.newFixedThreadPool(this.listenerResolver.getThreads());
 		int threads = this.listenerResolver.getThreads();
 		int maxQueueSize = this.listenerResolver.getMaxQueueSize();
-		executor = new ThreadPoolExecutor(threads, threads*2, 1, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>(maxQueueSize));
+		
 
+		executor = new ThreadPoolExecutor(threads, threads*2, 1, TimeUnit.MINUTES,
+				new LinkedBlockingQueue<Runnable>(maxQueueSize),//
+				new DefaultManagedAwareThreadFactory()) {
+			       protected void afterExecute(Runnable r, Throwable t) {
+				    super.afterExecute(r, t);
+				   printException(r, t);
+			    }
+		};
+		
 		if (consumerMap != null) {
 			//循环提交监听
 			for (String topic : consumerMap.keySet()) {
@@ -59,6 +74,25 @@ public class SubscriptionScheduler {
 				}
 			}
 		}
+	}
+	
+	
+	private static void printException(Runnable r, Throwable t) {   
+	    if (t == null && r instanceof Future<?>) {   
+	        try {   
+	            Future<?> future = (Future<?>) r;   
+	            if (future.isDone())   
+	                future.get();   
+	        } catch (CancellationException ce) {   
+	            t = ce;   
+	        } catch (ExecutionException ee) {   
+	            t = ee.getCause();   
+	        } catch (InterruptedException ie) {   
+	            Thread.currentThread().interrupt(); // ignore/reset   
+	        }   
+	    }   
+	    if (t != null)   
+	        logger.error(t.getMessage(), t);   
 	}
 
 
